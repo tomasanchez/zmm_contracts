9f9870b606dba8c1698ca02a127123e357fc2107
Tomas <tosanchez@frba.utn.edu.ar>
2021-07-30 11:33:44 -0300

    feat: add dialog handler

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -157,6 +157,139 @@ sap.ui.define(
         })(),
 
         /* =========================================================== */
+        /* fragments methods                                            */
+        /* =========================================================== */
+
+        /**
+         * Convenince method for openning a Dialog Fragment with a controller.
+         * @public
+         * @param {string} sName the fragment name
+         * @param {sap.ui.mvc.Model} model to be set(optional)
+         * @param {boolean} updateModelAllways let the function know if it has to update the model every time it opens the dialog or only the first time.
+         * @param {function} callbak a function in the controller from where it’s called which can be executed from the fragment controller
+         * @param {object} data passed from the main view to the fragment
+         * @author Tomas A Sanchez
+         * @since 07.30.2021
+         */
+        openDialog: function (sName, updateModelAlways, callback, data) {
+          /**
+           * the View name-space path
+           * @type {string}
+           */
+          var sViewPath = this.getViewPath(sName);
+
+          /**
+           * A generated ID
+           * @type {string}
+           */
+          var id = `${this.getView().getId()}-${sName}`;
+
+          if (!_fragments[id]) {
+            //create controller
+            let sControllerPath = sViewPath.replace("view", "controller");
+
+            _fragments[id] = { fragment: {}, controller: {} };
+
+            Controller.create({ name: sControllerPath + sName })
+              .then((oController) => {
+                _fragments[id].controller = oController;
+                this.createDialogFragment(
+                  id,
+                  sViewPath + sName,
+                  oController,
+                  model,
+                  updateModelAlways,
+                  callback,
+                  data
+                );
+              })
+              .catch((reason) => {
+                _fragments[id].controller = this;
+                this.createDialogFragment(
+                  id,
+                  sViewPath + sName,
+                  this,
+                  model,
+                  updateModelAlways,
+                  callback,
+                  data
+                );
+              });
+          } else _fragments[id].fragment.open();
+        },
+
+        /**
+         * Convenince method for closing all opened Fragments.
+         * @function
+         * @public
+         */
+        closeFragments: function () {
+          /*
+			This makes it easy for a close button in each fragment.
+      	     It just calls this function and it will close the open fragments. (In case the fragment contains a dialog.)
+		  */
+          for (var f in _fragments) {
+            if (
+              _fragments[f]["fragment"] &&
+              _fragments[f].fragment["isOpen"] &&
+              _fragments[f].fragment.isOpen()
+            ) {
+              _fragments[f].fragment.close();
+            }
+          }
+        },
+
+        /**
+         * Convenince method for getting an specific fragment
+         * @public
+         * @param {string} fragment name
+         */
+        getFragment: function (fragment) {
+          return _fragments[this.getView().getId() + "-" + fragment];
+        },
+
+        /**
+         * Convenince method for getting a control from in the fragment
+         * @public
+         * @param {sap.ui.mvc.Controller}
+         * @param {string} id of control
+         * Use example:
+         *	var oButton = this.fragmentById(this.parent,"button0");
+         */
+        fragmentById: function (parent, id) {
+          var latest = this.getMetadata().getName().split(".")[
+            this.getMetadata().getName().split(".").length - 1
+          ];
+          return sap.ui
+            .getCore()
+            .byId(parent.getView().getId() + "-" + latest + "--" + id);
+        },
+
+        /**
+         * Gets the view's namespace-path
+         * @param {string} sName
+         * @returns {string} the view path
+         */
+        getViewPath: function (sName) {
+          if (sName.indexOf(".") > 0) {
+            var aViewName = sName.split(".");
+            sName = sName.substr(sName.lastIndexOf(".") + 1);
+          } else {
+            //current folder
+            aViewName = this.getView().getViewName().split("."); // view.login.Login
+          }
+
+          aViewName.pop();
+          var sViewPath = aViewName.join("."); // view.login
+
+          if (sViewPath.toLowerCase().indexOf("fragments") > 0) {
+            sViewPath += ".";
+          } else {
+            sViewPath += ".fragments.";
+          }
+          return sViewPath;
+        },
+        /* =========================================================== */
         /* email methods                                               */
         /* =========================================================== */
 
@@ -172,6 +305,65 @@ sap.ui.define(
             oViewModel.getProperty("/shareSendEmailSubject"),
             oViewModel.getProperty("/shareSendEmailMessage")
           );
+        },
+
+        /**
+         * Creates a new Dialog.
+         * @private
+         * @param {string} sID the fragment ID
+         * @param {string} name the fragment view-name
+         * @param {sap.ui.mvc.Controller} oController the controller to be set on the fragment
+         * @param {sap.ui.model.Model} model an optional model to be set on the fragment
+         * @param {boolean} updateModelAllways an optional flag for updating the model allways
+         * @param {() => void} callback an optional callback function
+         * @param {object} data an optional object to be passed as data
+         */
+        createDialogFragment: function (
+          sID,
+          name,
+          oController,
+          model,
+          updateModelAllways,
+          callback,
+          data
+        ) {
+          _fragments[sID] = {
+            fragment: sap.ui.require(
+              ["sap/ui/core/Fragment"],
+              (fragmentClass) => {
+                fragmentClass
+                  .load({
+                    id: sID,
+                    name: name,
+                    controller: oController,
+                  })
+                  .then((oFragment) => {
+                    // version >= 1.20.x
+                    _fragments[sID].fragment = oFragment;
+                    this.getView().addDependent(oFragment);
+                    var fragment = oFragment;
+
+                    _fragments[sID].controller = oController;
+
+                    if (model && updateModelAllways) {
+                      fragment.setModel(model);
+                    }
+                    if (oController && oController !== this) {
+                      _fragments[sID].controller.onBeforeShow(
+                        oController,
+                        fragment,
+                        callback,
+                        data
+                      );
+                    }
+
+                    setTimeout(function () {
+                      fragment.open();
+                    }, 100);
+                  });
+              }
+            ),
+          };
         },
       }
     );
